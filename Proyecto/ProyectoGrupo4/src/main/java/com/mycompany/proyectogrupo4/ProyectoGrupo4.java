7c18dbb2a2ea87b2d5e086379c2fe85962254207
Cactuzito <jalvarado00090@ufide.ac.cr>
2026-07-25 16:47:28 -0600

    first commit

---
+++ Proyecto/ProyectoGrupo4/src/main/java/com/mycompany/proyectogrupo4/ProyectoGrupo4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectogrupo4;
+
+/**
+ *
+ * @author josue
+ */
+public class ProyectoGrupo4 {
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto Final");
+    }
+}
